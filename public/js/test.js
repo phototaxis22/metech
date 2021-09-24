570bc649023d8558c139d6400da389dc38e87094
phototaxis22 <akiko.shimamoto@sivaquen.com>
2021-09-24 16:04:05 +0900

    test

---
+++ public/js/test.js
@@ -0,0 +1,13 @@
+
+function ShowAlart() {
+    alert("Hello world!!");
+    }
+
+$(function(){
+    $("#header").load("/templates/header.html");
+
+    });
+
+    $(document).ready(function(){
+        $("p").css("color", "red");
+        });
